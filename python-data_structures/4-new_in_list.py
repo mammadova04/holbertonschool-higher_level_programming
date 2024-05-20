296efa0015dfaefdf255d7980fdf22065cb845c7
mammadova04 <nmammadova04@gmail.com>
2024-05-20 11:23:12 +0000

    made modifications

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list_1 = my_list
-    if idx >= 0 and idx < len(my_list):
+    my_list_1 = my_list[:]
+    if idx >= 0 and idx < len(my_list_1):
         my_list_1[idx] = element
     return my_list_1

25f2b8bfdaad74ec3be4dce6d20df9f412b1b241
mammadova04 <nmammadova04@gmail.com>
2024-05-30 12:55:39 +0000

    pycode

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+     
     def area(self):
         """Returns area of rectangle"""
         return self.width * self.height

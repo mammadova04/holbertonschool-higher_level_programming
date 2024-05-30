45ff664f9b621aad5dd05a807874efd969c2f721
mammadova04 <nmammadova04@gmail.com>
2024-05-30 19:28:53 +0000

    a function that divides all elements of a matrix.

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
-
+i
 matrix = [
     [1, 2, 3],
     [4, 5, 6]
@@ -8,6 +8,3 @@ matrix = [
 print(matrix_divided(matrix, 3))
 print(matrix)
 
-guillaume@ubuntu:~/$ ./2-main.py
-[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
-[[1, 2, 3], [4, 5, 6]]
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""matrix division and test"""
+
+
+def matrix_divided(matrix, div):
+    """matrix division"""
+    divisionerror = "division by zero"
+    typeerror = "div must be a number"
+    itemerror = "matrix must be a matrix (list of lists) of integers/floats"
+    sizeerror = "Each row of the matrix must have the same size"
+
+    def itemerrorfunc():
+        raise TypeError(itemerror)
+
+    def sizeerrorfunc():
+        raise TypeError(sizeerror)
+
+    if not isinstance(div, (int, float)):
+        raise TypeError(typeerror)
+    if div == 0 or div == 0.0:
+        raise ZeroDivisionError(divisionerror)
+
+    length = len(matrix[0])
+    return list(map(
+        lambda x: list(map(
+            lambda y: round(y / div, 2) if isinstance(
+                y, (float, int)
+                ) else itemerrorfunc(), x
+            )) if len(x) == length else sizeerrorfunc(), matrix
+        ))

c10d427678bb1ac8eb78ee8fe24d8ad955130e3c
mammadova04 <nmammadova04@gmail.com>
2024-05-31 12:47:28 +0000

    a class BaseGeometry (based on 6-base_geometry.py).

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Geometry module"""
+
+
+class BaseGeometry:
+    """Exception"""
+
+    def area(self):
+        """fucn is docced"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """fucn is docced"""
+
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

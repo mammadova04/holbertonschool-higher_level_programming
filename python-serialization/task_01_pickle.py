3c684aceaf0c099ee42cd39b4419444fa99bf452
mammadova04 <nmammadova04@gmail.com>
2024-06-06 19:32:57 +0000

    Pickling Custom Classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """
+        Serializes the current instance of the object\
+                and saves it to the provided filename.
+        """
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Error serializing object: {e}")
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Loads and returns an instance of CustomObject\
+                from the provided filename.
+        """
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception as e:
+            print(f"Error deserializing object: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    # Create an instance of CustomObject
+    obj = CustomObject(name="John", age=25, is_student=True)
+    print("Original Object:")
+    obj.display()
+
+    # Serialize the object
+    obj.serialize("object.pkl")
+
+    # Deserialize the object into a new instance
+    new_obj = CustomObject.deserialize("object.pkl")
+    if new_obj:
+        print("\nDeserialized Object:")
+        new_obj.display()

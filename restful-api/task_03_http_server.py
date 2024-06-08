575e83cbfd4dc46f2e1c86e998af70e6dab7a9f4
mammadova04 <nmammadova04@gmail.com>
2024-06-08 11:29:17 +0000

    modified

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def _set_headers(self, status_code=200, content_type='text/plain'):
-        self.send_response(status_code)
+    def _set_headers(self, content_type='text/html'):
+        self.send_response(200)
         self.send_header('Content-type', content_type)
         self.end_headers()
 
@@ -13,19 +12,17 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self._set_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
-            self._set_headers(200, 'application/json')
+            self._set_headers('application/json')
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self._set_headers()
             self.wfile.write(b"OK")
         else:
-            self._set_headers(404, 'text/plain')
-            self.wfile.write(b"Endpoint not found")
+            self.send_error(404, "Endpoint not found")
 
     def do_POST(self):
-        self._set_headers(405)
-        self.wfile.write(b"Method Not Allowed")
+        self.send_error(405, "Method Not Allowed")
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
@@ -35,4 +32,3 @@ def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=80
 
 if __name__ == "__main__":
     run()
-

857d520586327edf70d6828a13e1f1c5f4cc03f9
mammadova04 <nmammadova04@gmail.com>
2024-07-28 13:59:37 +0000

    Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const redHeader = document.getElementById('red_header');
+  const header = document.querySelector('header');
+
+  redHeader.addEventListener('click', function() {
+    header.style.color = '#FF0000';
+  });
+});
+
